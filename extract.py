ebc1ed475497c67052059c50834ca9abd3299e5c
Lulu <luhangxin@gmail.com>
2018-12-09 23:51:05 +0800

    scrape a current weibo account

--- extract.py
+++ extract.py
@@ -1,15 +1,24 @@
 import json 
 import pdb
+import urllib.request
+
 d_text = []
 d_pic = []
 
 with open("data/friends_timeline_20181123.out.json") as f:
-    d_json = json.load(f)
+	d_json = json.load(f)
+
+dog_num = 1
 
 for weibo in d_json["statuses"]:
-    d_text.append(weibo["text"])
-  #  d_pic.append(weibo["original_pic"])
+	#print(weibo["text"])
+	d_text.append(weibo["text"])
+	if "original_pic" in weibo.keys():
+		pic_addr = weibo["original_pic"]
+		#print(pic_addr)
+		#urllib.request.urlretrieve(pic_addr, "data/"+str(dog_num)+".jpg")
 
+	dog_num = dog_num + 1
 
-pdb.set_trace()
+#pdb.set_trace()
 print(d_text)
